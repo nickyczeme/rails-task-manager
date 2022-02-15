876c4896eb07e19ea4bfb75c5eff69ce5ce246a3
Nicole Czemerinski <nickyczeme@gmail.com>
2022-02-15 14:55:32 +0100

    task

--- db/migrate/20220215100659_create_tasks.rb
+++ db/migrate/20220215100659_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :title
       t.text :details
-      t.boolean :completed
+      t.boolean :completed, default: false
 
       t.timestamps
     end
